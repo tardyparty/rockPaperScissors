f12f42f005df20584901424adfab52457030dcb5
tardyparty <tardyparty@users.noreply.github.com>
2019-09-05 11:08:09 -0500

    initital page setup and linking

---
+++ javascript/app.js
@@ -0,0 +1,11 @@
+// - only two players online at the same time
+    // player picks a username
+    // player is online
+
+// -when only two players are online
+    // - players picks rock, paper, scissors. 
+    //  - after selection, game shows the outcome of the game
+
+// - game tracks each players wins / losses
+
+//  - players can chat while playing the game
