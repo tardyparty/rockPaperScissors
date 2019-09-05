f1aa53e4c844dd9be73073260086846ace46e130
tardyparty <tardyparty@users.noreply.github.com>
2019-09-05 11:35:07 -0500

    global variables & fontAwesome icons

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,26 @@
+// firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAcW6zFp8AyaRrHvPm3VJxfk1cSBbkguxQ",
+    authDomain: "rockpaperscissors-f19ad.firebaseapp.com",
+    databaseURL: "https://rockpaperscissors-f19ad.firebaseio.com",
+    projectId: "rockpaperscissors-f19ad",
+    storageBucket: "",
+    messagingSenderId: "1004885032983",
+    appId: "1:1004885032983:web:793cb07b9220d794ba9c52"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+
+// global variables 
+const rockImg = '<i class="far fa-hand-rock"></i>';
+const paperImg = '<i class="far fa-hand-paper"></i>';
+const scissorImg = '<i class="far fa-hand-scissors"></i>';
+
+// players
+let player1 = '';
+let player2 = '';
+
 // - only two players online at the same time
     // player picks a username
     // player is online
