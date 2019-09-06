d3285af81fdbcd2a19b6b529c871b7dc22f6ab42
tardyparty <tardyparty@users.noreply.github.com>
2019-09-06 17:16:00 -0500

    psuedocode

--- javascript/app.js
+++ javascript/app.js
@@ -13,22 +13,52 @@ var firebaseConfig = {
 
 
 // global variables 
-const rockImg = '<i class="far fa-hand-rock"></i>';
-const paperImg = '<i class="far fa-hand-paper"></i>';
-const scissorImg = '<i class="far fa-hand-scissors"></i>';
+const rockImg = '<i id="rock" class="far fa-hand-rock"></i>';
+const paperImg = '<i id="paper" class="far fa-hand-paper"></i>';
+const scissorImg = '<i id="scissors" class="far fa-hand-scissors"></i>';
+
+const ref = firebase.database().ref('/');
 
 // players
 let player1 = '';
 let player2 = '';
 
-// - only two players online at the same time
-    // player picks a username
-    // player is online
 
-// -when only two players are online
-    // - players picks rock, paper, scissors. 
-    //  - after selection, game shows the outcome of the game
+function username(){
+    $("#gameBox").prepend(`
+        <form>
+            <label id="username">Choose your name</label>
+            <input type="text">
+        </form>
+    `);
+}
 
-// - game tracks each players wins / losses
+username();
+
+
+// - once someone lands on the page - 
+    // choose a username
+    // notified in the chat box that they are now online and ready to play
+    // will see notification when someone else is online
+    // when 2 are online, ability to create username halts
+
+
+// - once two players are online -
+    // - each player can now click on rock paper or scissor
+        // - each choice is sent to firebase
+        // - once both have chosen, firebase sends both choices to the game() func to determine who won and prints the results
+        // - after game() returns winner/loser - 
+            // - gameReset() is automatically called to prepare for the next game
+            // - display "choose r/p/s only while users choice is unselected"
+
+// - Win/Losses
+    // - anytime the game ends, wins losses for each player will be updated
 
 //  - players can chat while playing the game
+    // recieve a message form player to firebase
+    // on child added to firebase from certain player, print that out on the chat under players name. 
+    // new chats should append to the bottom and not change the height of text box.
+    // add ability to scroll to top of chat
+
+// -  BONUS - 
+    // set up button for single player to play against a computer
